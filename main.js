54a31cd1f46b58e81aae7fcf5d960c3b5fd62c87
CertIanS <irsnider13@gmail.com>
2026-06-02 12:06:11 -0400

    Edited menu functionality

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 var menu = document.getElementById("menu");
 
-function openMenu(e){
-    e.preventDefault();
-    menu.classList.toggle("show");
+function openMenu(){
+    //e.preventDefault();
+    document.querySelector("header").style.filter = "brightness(50%)";
+    document.getElementById("newSection").style.filter = "brightness(50%)";
+    document.getElementById("bottomSection").style.filter = "brightness(50%)";
+    
+    document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    menu.classList.add("show");
 }
 
 function closeMenu(){
     menu.classList.remove("show");
+    document.body.style.backgroundColor = "white";
+    document.querySelector("header").style.filter = "brightness(100%)";
+    document.getElementById("newSection").style.filter = "brightness(100%)";
+    document.getElementById("bottomSection").style.filter = "brightness(100%)";
 }
