0af3200505f5133d719ecbeb39e2b97179e94c8b
CertIanS <irsnider13@gmail.com>
2026-06-02 13:33:32 -0400

    Refactored menu code

--- main.js
+++ main.js
@@ -2,10 +2,7 @@ var menu = document.getElementById("menu");
 
 function openMenu(){
     //e.preventDefault();
-    document.querySelector("header").style.filter = "brightness(50%)";
-    document.getElementById("newSection").style.filter = "brightness(50%)";
-    document.getElementById("bottomSection").style.filter = "brightness(50%)";
-    
+    document.querySelector("main").style.filter = "brightness(50%)";
     document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     menu.classList.add("show");
 }
@@ -13,7 +10,5 @@ function openMenu(){
 function closeMenu(){
     menu.classList.remove("show");
     document.body.style.backgroundColor = "white";
-    document.querySelector("header").style.filter = "brightness(100%)";
-    document.getElementById("newSection").style.filter = "brightness(100%)";
-    document.getElementById("bottomSection").style.filter = "brightness(100%)";
+    document.querySelector("main").style.filter = "brightness(100%)";
 }
